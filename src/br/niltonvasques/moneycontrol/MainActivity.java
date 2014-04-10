68af45c364a96d9ffa7b84a18d73e77ea9c3d979
Nilton Vasques <nilton.vasques@gmail.com>
2014-04-10 09:36:12 -0300

    Implementado a possibilidade de abrir o navigation através do ícone da action bar. Movido as strings para o arquivo strings.xml

--- src/br/niltonvasques/moneycontrol/MainActivity.java
+++ src/br/niltonvasques/moneycontrol/MainActivity.java
@@ -1,19 +1,17 @@
 package br.niltonvasques.moneycontrol;
 
-import java.text.SimpleDateFormat;
-import java.util.GregorianCalendar;
-
 import android.annotation.SuppressLint;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import br.niltonvasques.moneycontrol.activity.NVFragmentActivity;
 import br.niltonvasques.moneycontrol.app.MoneyControlApp;
 import br.niltonvasques.moneycontrol.database.DatabaseHandler;
@@ -22,50 +20,87 @@ import br.niltonvasques.moneycontrol.view.fragment.MainFragment;
 
 @SuppressLint("NewApi")
 public class MainActivity extends NVFragmentActivity {
-	
+
 	private static final String TAG = "[MainActivity]";
 
 	private MoneyControlApp app;
 	private DatabaseHandler db;
-	
+
 	private String[] mDrawerItens;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-	
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private ListView mDrawerList;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		app = (MoneyControlApp) getApplication();
-	
-		
-		mDrawerItens = getResources().getStringArray(R.array.planets_array);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_list_item, mDrawerItens));
-        
-        changeFragment( new MainFragment() );
-        getActionBar().setTitle(mDrawerItens[0]);
-        
-        mDrawerList.setOnItemClickListener(new OnItemClickListener() {
-        	@Override
-        	public void onItemClick(AdapterView<?> arg0, View arg1, int position,long arg3) {
-        		if(mDrawerItens[position].equals("Principal")){
-        			changeFragment( new MainFragment() );
-        		}else
-        		if(mDrawerItens[position].equals("Categorias")){
-        			changeFragment(new CategoriasFragment());
-        		}
-        		
-        		mDrawerLayout.closeDrawer(mDrawerList);
-        		getActionBar().setTitle(mDrawerItens[position]);
-        		
-        	}
+		app = (MoneyControlApp) getApplication();
+
+
+		mDrawerItens = getResources().getStringArray(R.array.planets_array);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		// Set the adapter for the list view
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_list_item, mDrawerItens));
+
+		changeFragment( new MainFragment() );
+		getActionBar().setTitle(mDrawerItens[0]);
+
+		mDrawerList.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position,long arg3) {
+				if(mDrawerItens[position].equals("Principal")){
+					changeFragment( new MainFragment() );
+				}else
+					if(mDrawerItens[position].equals("Categorias")){
+						changeFragment(new CategoriasFragment());
+					}
+
+				mDrawerLayout.closeDrawer(mDrawerList);
+				getActionBar().setTitle(mDrawerItens[position]);
+
+			}
 		});
-		
+
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+			/** Called when a drawer has settled in a completely closed state. */
+			public void onDrawerClosed(View view) {
+				super.onDrawerClosed(view);
+				//                getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+
+			/** Called when a drawer has settled in a completely open state. */
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+				//                getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+		};
+
+		// Set the drawer toggle as the DrawerListener
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
 	}
 
 	@Override
@@ -74,7 +109,23 @@ public class MainActivity extends NVFragmentActivity {
 		getMenuInflater().inflate(R.menu.main_activity_actions, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
-	
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// If the nav drawer is open, hide action items related to the content view
+		boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+		menu.findItem(R.id.action_add).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 	@Override
 	public int getFragmentContentID() {
 		return R.id.content_frame;
