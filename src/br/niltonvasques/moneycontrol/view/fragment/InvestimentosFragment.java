34925fb7cae9fb5b9f69a040fd6f0cbf1248b6c4
Nilton Vasques <nilton.vasques@gmail.com>
2015-05-14 20:33:00 -0300

    Remove option menu from investiment screen. #6

--- src/br/niltonvasques/moneycontrol/view/fragment/InvestimentosFragment.java
+++ src/br/niltonvasques/moneycontrol/view/fragment/InvestimentosFragment.java
@@ -135,7 +135,7 @@ public class InvestimentosFragment extends Fragment{
 	
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		inflater.inflate(R.menu.main_transacaoes_actions, menu);
+//		inflater.inflate(R.menu.main_transacaoes_actions, menu);
 		super.onCreateOptionsMenu(menu, inflater);
 	}
 	
