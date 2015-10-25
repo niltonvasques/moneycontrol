7a4f1c1934d674a7ff051665bd40100752c968a4
Nilton Vasques <nilton.vasques@gmail.com>
2015-10-25 20:06:13 -0300

    Add a file chooser to improve ux for save/restore backup. #54

--- code/moneyControl/src/main/java/br/niltonvasques/moneycontrol/view/fragment/BackupFragment.java
+++ code/moneyControl/src/main/java/br/niltonvasques/moneycontrol/view/fragment/BackupFragment.java
@@ -1,11 +1,16 @@
 package br.niltonvasques.moneycontrol.view.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.devpaul.filepickerlibrary.FilePickerActivity;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -58,47 +63,79 @@ public class BackupFragment extends Fragment{
 		db = app.getDatabase();
 
 		myFragmentView.findViewById(R.id.fragmentBackupBtnSaveBackup).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if(db.checkDataBase()){
-					File in = new File(db.getDbPath());
-					File out = new File(Environment.getExternalStorageDirectory(), "/moneycontrol.backup");
-                    System.out.println(in);
-                    System.out.println(out);
-					try {
-						FileInputStream fis = new FileInputStream(in);
-						FileOutputStream fos = new FileOutputStream(out);
-						FileUtils.copyFile(fis, fos);
-					} catch (FileNotFoundException e) {
-						e.printStackTrace();
-						MessageUtils.showDefaultErrorMessage(getActivity());
-					} catch (IOException e) {
-						e.printStackTrace();
-						MessageUtils.showDefaultErrorMessage(getActivity());
-					}
-				}else{
-					MessageUtils.showDefaultErrorMessage(getActivity());
-				}
-			}
-		});
+            @Override
+            public void onClick(View v) {
+                if (db.checkDataBase()) {
+                    Intent filePickerIntent = new Intent(getActivity(), FilePickerActivity.class);
+                    filePickerIntent.putExtra(FilePickerActivity.REQUEST_CODE, FilePickerActivity.REQUEST_DIRECTORY);
+                    startActivityForResult(filePickerIntent, FilePickerActivity.REQUEST_DIRECTORY);
+                } else {
+                    MessageUtils.showDefaultErrorMessage(getActivity());
+                }
+            }
+        });
 
 		myFragmentView.findViewById(R.id.fragmentBackupBtnRestoreBackup).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
+                Intent filePickerIntent = new Intent(getActivity(), FilePickerActivity.class);
+                filePickerIntent.putExtra(FilePickerActivity.REQUEST_CODE, FilePickerActivity.REQUEST_FILE);
+                startActivityForResult(filePickerIntent, FilePickerActivity.REQUEST_FILE);
+            }
+        });
+
+		return myFragmentView;
+	}
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == FilePickerActivity.REQUEST_DIRECTORY
+                && resultCode == Activity.RESULT_OK) {
+
+            String filePath = data.
+                    getStringExtra(FilePickerActivity.FILE_EXTRA_DATA_PATH);
+            if(filePath != null) {
+                //do something with filePath...
+//                Toast.makeText(getActivity(), "Choose: "+filePath, Toast.LENGTH_LONG).show();
+                File in = new File(db.getDbPath());
+                File out = new File(filePath+"/moneycontrol.mcbk");
+                System.out.println(in);
+                System.out.println(out);
                 try {
-                    File out = new File(Environment.getExternalStorageDirectory(), "/moneycontrol.backup");
-                    if(!out.exists() || !db.importDatabase(out.getPath())){
+                    FileInputStream fis = new FileInputStream(in);
+                    FileOutputStream fos = new FileOutputStream(out);
+                    FileUtils.copyFile(fis, fos);
+                    MessageUtils.showMessage(getActivity(), app.getString(R.string.fragment_backup_success_title), app.getString(R.string.fragment_backup_success_message)+filePath);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    MessageUtils.showDefaultErrorMessage(getActivity());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    MessageUtils.showDefaultErrorMessage(getActivity());
+                }
+            }
+        }
+        else if(requestCode == FilePickerActivity.REQUEST_FILE
+                && resultCode == Activity.RESULT_OK) {
+            String filePath = data.
+                    getStringExtra(FilePickerActivity.FILE_EXTRA_DATA_PATH);
+            System.out.println("Request file result");
+            if(filePath != null) {
+                try {
+                    File out = new File(filePath);
+                    if (!filePath.contains(".mcbk") || !out.exists() || !db.importDatabase(out.getPath())) {
                         MessageUtils.showDefaultErrorMessage(getActivity());
+                    }else{
+                        MessageUtils.showMessage(getActivity(), app.getString(R.string.fragment_backup_success_title), app.getString(R.string.fragment_backup_success_restore));
                     }
                 } catch (IOException e) {
                     MessageUtils.showDefaultErrorMessage(getActivity());
                     e.printStackTrace();
                 }
+            }else{
+                MessageUtils.showDefaultErrorMessage(getActivity());
             }
-		});
-
-		return myFragmentView;
-	}
-
-
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
